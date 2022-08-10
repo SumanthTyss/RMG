20e9c867c8c47a9537c2103720014cf01624a022
Harisha M V <Harisha M V@HarishaMV>
2022-08-10 17:14:52 +0530

    commit

--- src/test/java/com/crm/CreateAdmin/AdminAndVerify.java
+++ src/test/java/com/crm/CreateAdmin/AdminAndVerify.java
@@ -1,5 +1,14 @@
 package com.crm.CreateAdmin;
 
+import org.testng.annotations.Test;
+
 public class AdminAndVerify {
+	
+	@Test
+	public void ToCheckForIndividualRoomRegistrationAsOccupied() {
+		
+		
+		
+	}
 
 }
